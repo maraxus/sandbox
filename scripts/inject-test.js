fe10cf938bdcd565a27a21a0c7e9764405ebc83a
Rodrigo <maraxus@localhost.localdomain>
2017-05-26 11:02:38 -0300

    h2 instead of h1

--- scripts/inject-test.js
+++ scripts/inject-test.js
@@ -1,3 +1,3 @@
-var count = document.getElementsByTagName('h1').length || "just one or none";
+var count = " " + document.getElementsByTagName('h2').length || "just one or none";
 var span = document.createElement('span').appendChild(document.createTextNode(count));
-document.getElementsByTagName(h1)[0].appendChild(span);
+document.getElementsByTagName('h2')[0].appendChild(span);
